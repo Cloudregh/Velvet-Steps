ecbb13fec1d516cd48551a9f7d62bdb533a4ab45
Cloudregh <emmacloudre@gmail.com>
2026-01-17 19:02:02 +0000

    feat: Implement product display component, correcting image imports and integrating it into the application.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Star } from 'lucide-react'
-import {Block} from "./assets/Block.png"
-import {Espadrilles} from "./assets/Espadrilles.png"
-import {Gladiator} from "./assets/Gladiator.png"
-import {Heeled} from "./assets/Heeled.png"
-import {Knee_high_Boots} from "./assets/Knee_high_Boots.png"
-import {Fuzzy_Slippers} from "./assets/Fuzzy_Slippers.png"
+import Block from "../assets/Block.png"
+import Espadrilles from "../assets/Espadrilles.png"
+import Gladiator from "../assets/Gladiator.png"
+import Heeled from "../assets/Heeled.png"
+import Knee_high_Boots from "../assets/Knee_high_Boots.png"
+import Fuzzy_Slippers from "../assets/Fuzzy_Slippers.png"
 
 
 
 const products = [
-    { id:1, name:"Block Heels Sandals", price:300, rating:5, image:{Block}},
-    { id:2, name:"Heeled sandals", price:450, rating:5, image:{Heeled}},
-    { id:3, name:"Gladiator Sandals", price:250, rating:5, image:{Gladiator}},
-    { id:4, name:"Knee-high Boots", price:320, rating:5, image:{Knee_high_Boots}},
-    { id:5, name:"Espadrilles", price:280, rating:5, image:{Espadrilles}},
-    { id:6, name:"Fuzzy Slippers", price:380, rating:5, image:{Fuzzy_Slippers}},
+    { id:1, name:"Block Heels Sandals", price:300, rating:5, image:Block},
+    { id:2, name:"Heeled sandals", price:450, rating:5, image:Heeled},
+    { id:3, name:"Gladiator Sandals", price:250, rating:5, image:Gladiator},
+    { id:4, name:"Knee-high Boots", price:320, rating:5, image:Knee_high_Boots},
+    { id:5, name:"Espadrilles", price:280, rating:5, image:Espadrilles},
+    { id:6, name:"Fuzzy Slippers", price:380, rating:5, image:Fuzzy_Slippers},
 
 ];
 
@@ -25,7 +25,36 @@ export const Products = () => {
       <div className='absolute top-0 left-0 w-full h-full text-center text-8xl font-playfair-sc text-[#E7E4E4] select-none z-0 tracking-widest pointer-events-none'>
           PRODUCTS OF THE SEASON
       </div>
+
+      <div className='container mx-auto px-8 relative z-10'>
+        <h2 className='text-center text-3xl font-playfair text-[#8B4302] mb-12'>
+           Step into Style
+        </h2>
+      
+
+       <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        {products.map((product)=>(
+          <div key={product.id} className='group cursor-pointer'>
+            <div className='bg-[#FFFBF9] h-80 w-full mb-4 flex items-center justify-center overflow-hidden'>
+              <img src={product.image} alt={product.name}
+              className='w-full h-full object-cover transition-transform duration-300 group-hover:scale-105' />
+            </div>
+             <div className='flex justify-between items-start'>
+              <div>
+                <h3 className='text-lg font-merriweather font-regular text-[#8B4302]'>{product.name}</h3>
+                <div className='flex text-[#8B4302] mt-1'>
+                  {[...Array(5)].map((_, index) => (
+                    <Star key={index} className={`w-4 h-4 ${index < product.rating ? `fill-current`: 'text-[#8B4302]'}`}/>
+                  ))}
+                </div>
+              </div>
+              <span className='text-lg font-bold text-[#8B4302]'> Ghc {product.price}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 export default Products;
